284634fe160ca3ece64ba2c21a8ca81dc6a16b8f
Ivan Martinez <alcance.enlinea@gmail.com>
2014-10-30 16:08:13 -0700

    model fix url

--- blogengine/urls.py
+++ blogengine/urls.py
@@ -1,10 +1,15 @@
 from django.conf.urls import patterns, url
 from django.views.generic import ListView
-
-from blogengine.models import Post
+from .models import Post
 
 urlpatterns = patterns(
     '',
     # Index
-    url(r'^$', ListView.as_view(model=Post)),
+    url(
+        r'^$', ListView.as_view(
+            model=Post,
+            paginate_by=2,
+        ),
+        name='home'
+    ),
 )
