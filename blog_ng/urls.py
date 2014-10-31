789738060153e997806dc79c1a8e0cc4b1028404
Ivan Martinez <alcance.enlinea@gmail.com>
2014-10-30 21:56:49 -0700

    Pagination added

--- blog_ng/urls.py
+++ blog_ng/urls.py
@@ -6,11 +6,11 @@ admin.autodiscover()
 urlpatterns = patterns(
     '',
     # Examples:
-    # url(r'^$', 'blog_ng.views.home', name='home'),
+    # url(r'^$', 'django_tutorial_blog_ng.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
 
     # Blog URLs
-    url(r'^.*$', include('blogengine.urls')),
+    url(r'', include('blogengine.urls')),
 )
