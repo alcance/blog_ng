f7e606b195d425266eab32e3c4db8bd3f27bf200
Ivan Martinez <alcance.enlinea@gmail.com>
2014-10-30 23:50:21 -0700

    test_post_create and get_absolute_url method

--- blogengine/admin.py
+++ blogengine/admin.py
@@ -1,4 +1,8 @@
 import models
 from django.contrib import admin
 
+
+class PostAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+
 admin.site.register(models.Post)
--- blogengine/models.py
+++ blogengine/models.py
@@ -5,6 +5,13 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     pub_date = models.DateTimeField()
     text = models.TextField()
+    slug = models.SlugField()
+
+    def get_absolute_url(self):
+        return "/%s/%s/%s/" % (self.pub_date, self.pub_date.month, self.slug)
 
     def __unicode__(self):
         return self.title
+
+    class Meta:
+        ordering = ["-pub_date"]
